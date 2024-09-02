0b0ae21f32d9f3c59e6cbc836e43125e16164608
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-09-02 19:26:37 +0100

    Updated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module provides a function to calculate the start and end indexes 
+This module provides a function to calculate the start and end indexes
 for paginating data based on the given page number and page size.
 """
 
@@ -14,7 +14,8 @@ def index_range(page, page_size):
         page_size (int): The number of items per page.
 
     Returns:
-        tuple: A tuple containing the start index and end index for the given page.
+        tuple: A tuple containing the start index and end index for the given
+        page.
     """
     start_page = (page - 1) * page_size
     end_page = start_page + page_size
